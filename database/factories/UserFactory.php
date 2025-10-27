94154f1d6c8e994891b74e81f59c1b1914bddf0c
Bernard Omuse <79845295+omu34@users.noreply.github.com>
2025-10-27 23:52:32 +0300

    user fatory works

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,18 +21,19 @@ class UserFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
-    {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= 'password',
-            'remember_token' => Str::random(10),
-            'two_factor_secret' => Str::random(10),
-            'two_factor_recovery_codes' => Str::random(10),
-            'two_factor_confirmed_at' => now(),
-        ];
-    }
+{
+    return [
+        'name' => $this->faker->name(),
+        'email' => $this->faker->unique()->safeEmail(),
+        'email_verified_at' => now(),
+        'password' => static::$password ??= 'password',
+        'remember_token' => Str::random(10),
+        'two_factor_secret' => Str::random(10),
+        'two_factor_recovery_codes' => Str::random(10),
+        'two_factor_confirmed_at' => now(),
+    ];
+}
+
 
     /**
      * Indicate that the model's email address should be unverified.
