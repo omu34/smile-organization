41ddc721f53d76f8c1243d07aaa7edea02d6f30e
Bernard Omuse <bernardomuse22@gmail.com>
2026-01-02 20:39:41 +0300

    inncreased maximum-execution-time to 120

---
+++ storage/framework/views/d6502196e34d4bd651278155dfc99f25.php
@@ -0,0 +1,5 @@
+<div <?php echo e($attributes->class(['fi-dropdown-list'])); ?>>
+    <?php echo e($slot); ?>
+
+</div>
+<?php /**PATH C:\Users\Bernard O\Downloads\herd-projects\smile-with-ai\vendor\filament\support\resources\views/components/dropdown/list/index.blade.php ENDPATH**/ ?>
